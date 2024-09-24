a054be875a74ee628258b57f4443a384e63f51e2
ornobaadi <aadi4789@gmail.com>
2024-09-24 19:40:41 +0600

    Alhamdulillah

--- js/feni.js
+++ js/feni.js
@@ -10,31 +10,31 @@ document.getElementById('feni-donate-btn')
         const balance = getTextValue('account-balance');
 
 
-        if(isNaN(addMoney)){
+        if (isNaN(addMoney)) {
             alert('Failed to donate money');
             return;
         }
 
-        if(addMoney <= 0){
+        if (addMoney <= 0) {
             alert('Write the correct amount you want to donate');
             return;
         }
 
-        else{
-            
+        else {
+
             const newDonate = donate + addMoney;
 
             document.getElementById('feni-account').innerText = newDonate;
 
         }
 
-        if(addMoney > balance){
-            
+        if (addMoney > balance) {
+
             alert('You do not have enough money to donate');
             return;
         }
 
-        else{
+        else {
             const newBalance = balance - addMoney;
 
             document.getElementById('account-balance').innerText = newBalance;
@@ -42,10 +42,11 @@ document.getElementById('feni-donate-btn')
             document.getElementById('my_modal_1').showModal();
 
             const div = document.createElement('div');
-            div.className = "border rounded-[16px] p-4 space-y-4"
+            div.className = "border rounded-[16px] p-8 space-y-4"
             div.innerHTML = `
-                <h4 class="text-2xl font-bold">${addMoney} Taka is Donate for Flood Relief in Feni,Bangladesh </h4>
-                <p class="text-gray-500 font-light text-base bg-slate-50 rounded-lg p-2" >Date: ${new Date().toLocaleDateString()} Time: ${new Date().toLocaleTimeString('en-US', { timeZone: 'Asia/Dhaka' })}</p>
+                <h4 class="text-2xl px-2 font-bold">${addMoney} Taka is Donated for Flood Relief in Feni,Bangladesh </h4>
+                <p class="text-gray-500 font-light text-base bg-slate-50 rounded-lg p-2">
+                Date: ${new Date().toString()}</p>
             `
 
             document.getElementById('history-container').appendChild(div);
--- js/noakhali.js
+++ js/noakhali.js
@@ -42,10 +42,11 @@ document.getElementById('noakhali-donate-btn')
             document.getElementById('my_modal_1').showModal();
 
             const div = document.createElement('div');
-            div.className = "border rounded-[16px] p-4 space-y-4"
+            div.className = "border rounded-[16px] p-8 space-y-4"
             div.innerHTML = `
-                <h4 class="text-2xl font-bold">${addMoney} Taka is Donate for Flood Relief in Noakhali, Bangladesh </h4>
-                <p class="text-gray-500 font-light text-base bg-slate-50 rounded-lg p-2" >Date: ${new Date().toLocaleDateString()} Time: ${new Date().toLocaleTimeString('en-US', { timeZone: 'Asia/Dhaka' })}</p>
+                <h4 class="text-2xl px-2 font-bold">${addMoney} Taka is Donated for Flood Relief in Noakhali, Bangladesh </h4>
+                <p class="text-gray-500 font-light text-base bg-slate-50 rounded-lg p-2">
+                Date: ${new Date().toString()}</p>
             `
 
             document.getElementById('history-container').appendChild(div);
